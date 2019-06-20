b67183871e059c47a9cc52e8e1e9d9d4c7815419
Soren Klintrup <soren.klintrup@lego.com>
2019-06-20 08:35:59 +0200

    Using tr to replace the string-output from camcontrol, for a more human-readable script, no changes in functionality

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [1.3.0] - 2007-01-13
 ### Added
 - Initial public release
+## [1.3.1] - 2007-01-19
+### Added
+- Using tr to replace the string-output from camcontrol, for a more human-readable script, no changes in functionality.
--- check_smartarray.sh
+++ check_smartarray.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # NRPE check for Proliant SmartArray Controllers (ciss)
 # Written by: Søren Klintrup <soren at klintrup.dk>
-# version 1.3.0
+# version 1.3.1
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 DEVICES=$(camcontrol devlist|grep "COMPAQ"|sed -Ee 's/.*(pass[0-9]{1,3}).*/\1/')
@@ -12,27 +12,27 @@ for DEVICE in ${DEVICES}
 do
  DEVICENAME="$(camcontrol devlist|grep ${DEVICE}|sed -Ee 's/.*(da[0-9]{1,3}).*/\1/')"
  DEVICESTRING=$(camcontrol inquiry ${DEVICE} -D|cut -d '<' -f 2|cut -d '>' -f 1)
- case $(echo ${DEVICESTRING}|sed -Ee 's/.*([Rr][Ee][Aa]|[Ii][Nn][Tt]|[Rr][Ee][Cc]|[Ff][Aa][Ii]|[Oo][Kk]).*/\1/') in
-  [Ii][Nn][Tt])
+ case $(echo ${DEVICESTRING}|tr A-Z a-z|sed -Ee 's/.*(rea|int|rec|fai|ok).*/\1/') in
+  int)
    ERR=2
    ERRORSTRING="${ERRORSTRING} | ${DEVICENAME}: DEGRADED"
    ;;
-  [Ff][Aa][Ii])
+  fai)
    ERR=2
    ERRORSTRING="${ERRORSTRING} | ${DEVICENAME}: FAILED"
    ;;
-  [Rr][Ee][Cc])
+  rec)
    if ! [ "${ERR}" = 2 ]; then ERR=1;fi
    ERRORSTRING="${ERRORSTRING} | ${DEVICENAME}: rebuilding"
    ;;
-  [Rr][Ee][Aa])
+  rea)
    if ! [ "${ERR}" = 2 ]; then ERR=1;fi
    ERRORSTRING="${ERRORSTRING} | ${DEVICENAME}: ready for recovery"
    ;;
-  [Oo][Kk])
+  ok)
    OKSTRING="${OKSTRING} | ${DEVICENAME}: ok"
    ;;
- esac
+  esac
 done
 if [ "${ERRORSTRING}" -o "${OKSTRING}" ]
 then
